33fc0506569386b8f0d1b5e869bdc88cfbd4ac3e
Kevin van den Hoek <k.vandenhoek@wearetriple.com>
2021-10-28 12:56:53 +0200

    initial commit

---
+++ Resolver Workshop/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Resolver Workshop
+//
+//  Created by Kevin van den Hoek on 28/10/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Resolver WorkshopUITests/Resolver_WorkshopUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Resolver_WorkshopUITestsLaunchTests.swift
+//  Resolver WorkshopUITests
+//
+//  Created by Kevin van den Hoek on 28/10/2021.
+//
+
+import XCTest
+
+class Resolver_WorkshopUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
